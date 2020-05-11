d0d7fd2c5d63a492f2cd242f9f11d4377cfaf449
harsh <harsh.gupta4@learner.manipal.edu>
2020-05-11 16:19:31 +0530

    User proflie updat

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from PIL import Image
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        image = Image.open(self.img.path)
+
+        if image.height > 300 or image.width > 300:
+            output_size = (300, 300)
+            image.thumbnail(output_size)
+            image.save(self.img .path)
